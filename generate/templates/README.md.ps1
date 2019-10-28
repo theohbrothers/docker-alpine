ab132f125023d8f9b3c8325d5f237437ff70ee69
Leo <leonardjonathanoh@live.com>
2019-10-28 18:25:54 +0000

    Update readme with docker-image-layers badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -4,7 +4,7 @@
 [![pipeline status](https://gitlab.com/leojonathanoh/docker-alpine/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-alpine/commits/dev)
 [![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-alpine)](https://github.com/leojonathanoh/docker-alpine/releases/)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-alpine)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-alpine)
+[![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-alpine/latest)](https://hub.docker.com/r/leojonathanoh/docker-alpine)
 
 | Tags |
 |:-------:| $( $VARIANTS | % {
