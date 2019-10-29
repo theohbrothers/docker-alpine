f0e191a270d50062c944180d359a9defd41d044a
Leo <leonardjonathanoh@live.com>
2019-10-29 19:56:19 +0000

    Fix and regenerate .github/workflows/build.yml

--- generate/templates/.github/workflows/build.yml.ps1
+++ generate/templates/.github/workflows/build.yml.ps1
@@ -11,32 +11,60 @@ on:
     branches:
     - '**'
 
-.build_definition: &build_definition
-  steps:
-  - uses: actions/checkout@v1
-  - name: Display system info (linux)
-    run: |
-      set -e
-      hostname
-      whoami
-      cat /etc/*release
-      lscpu
-      free
-      df -h
-      pwd
-      docker info
-      docker version
-  - name: Login to docker registry
-    run: echo "${DOCKERHUB_REGISTRY_PASSWORD}" | docker login -u "${DOCKERHUB_REGISTRY_USER}" --password-stdin
-    env:
-      DOCKERHUB_REGISTRY_USER: ${{ secrets.DOCKERHUB_REGISTRY_USER }}
-      DOCKERHUB_REGISTRY_PASSWORD: ${{ secrets.DOCKERHUB_REGISTRY_PASSWORD }}
-  - name: Clean-up
-    run: docker logout
-    if: always()
-
 jobs:
-  alpine:
-    runs-on: ubuntu-18.04
-    <<: *build_definition
 '@
+
+$( $VARIANTS | % {
+@"
+
+  build-$( $_['tag'] ):
+    runs-on: ubuntu-18.04
+    env:
+      VARIANT_TAG: $( $_['tag'] )
+      VARIANT_TAG_WITH_VERSION: $( $_['tag'] )-`${{ github.ref }}
+      VARIANT_BUILD_DIR: $( $_['build_dir_rel'] )
+      CI_PROJECT_PATH: `${{ github.repository }}
+"@
+@'
+
+    steps:
+    - uses: actions/checkout@v1
+    - name: Display system info (linux)
+      run: |
+        set -e
+        hostname
+        whoami
+        cat /etc/*release
+        lscpu
+        free
+        df -h
+        pwd
+        docker info
+        docker version
+    - name: Login to docker registry
+      run: echo "${DOCKERHUB_REGISTRY_PASSWORD}" | docker login -u "${DOCKERHUB_REGISTRY_USER}" --password-stdin
+      env:
+        DOCKERHUB_REGISTRY_USER: ${{ secrets.DOCKERHUB_REGISTRY_USER }}
+        DOCKERHUB_REGISTRY_PASSWORD: ${{ secrets.DOCKERHUB_REGISTRY_PASSWORD }}
+    - name: Build and push image
+      env:
+        DOCKERHUB_REGISTRY_USER: ${{ secrets.DOCKERHUB_REGISTRY_USER }}
+      run: |
+        set -e
+
+        # Get 'project-name' from 'namespace/project-name'
+        CI_PROJECT_NAME=$( echo "${CI_PROJECT_PATH}" | cut -d '/' -f 2 )
+
+        docker build \
+          -t "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:${VARIANT_TAG}" \
+          -t "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:${VARIANT_TAG_WITH_VERSION}" \
+          -t "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:latest" \
+          "${VARIANT_BUILD_DIR}"
+        docker push "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:${VARIANT_TAG}"
+        docker push "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:${VARIANT_TAG_WITH_VERSION}"
+        docker push "${DOCKERHUB_REGISTRY_USER}/${CI_PROJECT_NAME}:latest"
+    - name: Clean-up
+      run: docker logout
+      if: always()
+'@
+})
