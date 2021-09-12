f1452cd4049ca7336b489c6e520c73968d598361
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-09-12 20:50:40 +0000

    Docs (readme): Replace tag badge with release badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -2,7 +2,7 @@
 # docker-alpine
 
 [![github-actions](https://github.com/theohbrothers/docker-alpine/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-alpine/actions)
-[![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-alpine)](https://github.com/theohbrothers/docker-alpine/releases/)
+[![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-alpine?style=flat-square)](https://github.com/theohbrothers/docker-alpine/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-alpine/latest)](https://hub.docker.com/r/theohbrothers/docker-alpine)
 
 Dockerized alpine with useful tools.
