70f6e1c54062239510d802991a8981fdae0e98fb
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-10-12 07:01:09 +0000

    Enhancement: Add curl component to mysqlclient-openssl variant
    This allows the image to be used to send notifications.

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -13,7 +13,7 @@ $local:VARIANTS_MATRIX = @(
             distro_version = $v
             subvariants = @(
                 @{ components = @( 'curl', 'git', 'jq', 'ssh' ); tag_as_latest = if ($v -eq $local:VARIANTS_DISTRO_VERSIONS[0]) { $true } else { $false } }
-                @{ components = @( 'mysqlclient', 'openssl' ) }
+                @{ components = @( 'curl', 'mysqlclient', 'openssl' ) }
             )
         }
     }
